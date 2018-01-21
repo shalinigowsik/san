27e4ea099022aff338644f6f100aac05936c9e71
shalinigowsik <31505574+shalinigowsik@users.noreply.github.com>
2018-01-21 11:19:25 +0530

    Create alph.c

---
+++ alph.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	char ch;
+	printf("input a character\n");
+	scanf("%c",&ch);
+	if((ch>='a' && ch>='z')||(ch>='a' && ch>='z'))
+	{
+	printf("alphabet");
+	}
+	else
+	{
+	printf("no";)
+	}
+	return 0;
+}
