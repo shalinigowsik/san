adb296986874a5a13b086e52500930af2c91f948
shalinigowsik <31505574+shalinigowsik@users.noreply.github.com>
2018-01-21 11:52:12 +0530

    Create ps.c

---
+++ ps.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int n,i,count=0;
+	printf("enter a number");
+	scanf("%d",&n);
+	for(i=2;i<n;i++)
+	{
+		if(n%i==0)
+		{
+			count++;
+		}
+		if(count==0)
+		printf("yes");
+		else
+		printf("no");
+	}
+	return 0;
+}
