8bcdb4a804a0eb1449f88fde10d9c0b873b7b2ca
shalinigowsik <31505574+shalinigowsik@users.noreply.github.com>
2018-01-21 10:53:58 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int i,n,sum;
+	printf("enter the value");
+	scanf("%d%d%d",&i,&n,&sum);
+	for(i=1;i<=n;i++)
+	{
+		sum=sum+i;
+	}
+	printf("the sum of the number is%d,sum");
+	return 0;
+}
