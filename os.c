eee0a4b013d935a84b48719ef89996803eca79f3
shalinigowsik <31505574+shalinigowsik@users.noreply.github.com>
2018-01-21 12:13:16 +0530

    Rename prog15.c to os.c

---
+++ os.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b,rem,i;
+printf("give the first number for the range:|n");
+scanf("%d,&a);
+printf("give the final number for the range :|n");
+scanf("%d",n&b);
+printf("|n the even number between %d and %d are ",a,b);
+for (i=a;i<=b;++i)
+{
+rem=i%2;
+if(rem==0)
+printf("\n%d",i);
+}
+return o;
+}
