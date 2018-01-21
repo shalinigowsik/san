cbb3f9a5a709fac6d5a281094295293a36421b21
shalinigowsik <31505574+shalinigowsik@users.noreply.github.com>
2018-01-21 12:26:27 +0530

    Create sub.c

---
+++ sub.c
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,s,res=0,rem;
+scanf("%d",&i);
+s=i;
+while(i!=0)
+{
+rem=i%10;
+res=res+d;
+i=i/10;
+}
+if(s==res)
+{
+printf("yes");
+}
+else
+{
+	printf("no");
+}
+}
