131d30f579770fceb093d09da009d0803019eca5
shalinigowsik <31505574+shalinigowsik@users.noreply.github.com>
2018-01-24 16:42:12 +0530

    Create res.c

---
+++ res.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int a=[15];
+int s,i,m;
+printf("enter the size");
+scanf("%d",&s);
+printf("enter the array");
+for(i=0;i<s;i++);
+{
+scanf("%d",&a[i])
+}
+max=max[0]
+for(i=0;i,s;i++)
+{
+if(maxa[i])
+max=a[i];
+pritf("enter the maxmium value");
+}
+return 0;
+}
