80d10ff80772f3f1fce236baa69bd28baa02c49b
shalinigowsik <31505574+shalinigowsik@users.noreply.github.com>
2018-01-27 15:44:19 +0530

    Create pro24.c

---
+++ pro24.c
@@ -0,0 +1,28 @@
+
+ #include<stdio.h>
+void main()
+{
+int i,j,n,t,a[i],a[j];
+printf("enter the element"):
+scanf("%d",&n);
+printf("enter the number"):
+for(i=0;i<n;i++)
+{
+scanf("%d",&n);
+}
+for(j=i+n;j<n;j++)
+{
+if(a[i]>[j])
+{
+t=a[i];
+a[i]=a[j];
+a[i]=t;
+}
+}
+printf("sorted the element");
+for(i=0;i<n;i++)
+{
+printf("%d",&n);
+}
+}
+
