7fc5bcefde43b9df8e008d3076b9b168d4f40116
shalinigowsik <31505574+shalinigowsik@users.noreply.github.com>
2018-02-07 10:51:18 +0000

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int f1=0,f2=0,f,n,i;
+	printf("enter the values");
+	scanf("%d",&n);
+	for(i=2;i<=2;i++)
+	{
+		f=f1+f2;
+		printf("%d",f);
+		f1=f2;
+		f2=f;
+	}
+	getch();
+}
