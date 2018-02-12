28eb2114d331b179abf22e78bdceeefc6c76590a
shalinigowsik <31505574+shalinigowsik@users.noreply.github.com>
2018-02-12 11:11:38 +0000

    Create kprint.c

---
+++ kprint.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char a[10];
+    int i,m;
+    printf("enter the string\n");
+    scanf("%s",&a);
+    printf("enter the no of times u need to repeat ur string");
+    scanf("%d",&m);
+    for(i=0;i<m;i++)
+    {
+        printf("%s",a);
+    }
+getch();
+}
