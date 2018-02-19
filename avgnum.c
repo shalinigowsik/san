a5bdcefdc3978ddd0c970a052ca9d055b33a2ea9
shalinigowsik <31505574+shalinigowsik@users.noreply.github.com>
2018-02-19 16:33:55 +0530

    Create avgnum.c

---
+++ avgnum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a[10],n,i,sum=0,b;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+    scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+sum=sum+a[i];
+b=sum/n;
+}
+printf("%d",b);
+return 0;
+}
