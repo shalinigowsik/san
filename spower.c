2219f319f92c281dcad9bdee1b48025595142d4b
shalinigowsik <31505574+shalinigowsik@users.noreply.github.com>
2018-02-19 16:50:22 +0530

    Rename power.c to spower.c

---
+++ spower.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	if(a%2==0)
+	{
+		printf("the given number is a power of 2");
+	}
+	else
+	{
+		printf("the given number  is not a power of 2");
+	}
+	return 0;
+}
