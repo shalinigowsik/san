f6705332923571da7e2260c8de14aa10d6ad5f43
shalinigowsik <31505574+shalinigowsik@users.noreply.github.com>
2018-03-14 16:05:55 +0530

    Create largest10.c

---
+++ largest10.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int a[20],i,max;
+for(i=0;i<10;i++)
+{
+scanf("%d",&a[i]);
+}
+max=a[0];
+for(i=0;i<10;i++)
+{
+if(max<a[i])
+{
+max=a[i];
+}
+}
+printf("%d",max);
+}
