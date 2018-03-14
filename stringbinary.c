3e61f69dd1d32c994424cd76076117496c721e36
shalinigowsik <31505574+shalinigowsik@users.noreply.github.com>
2018-03-14 16:28:54 +0530

    Create stringbinary.c

---
+++ stringbinary.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char a[100],i;
+printf("enter the string\n");
+scanf("%s",&a);
+if(a[i]=='1'||a[i]=='0')
+{
+printf("yes");
+}
+else
+{
+printf("not binary");
+}
+}
