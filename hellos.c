5cb55e36352e5487cbe4c6030818fb95b3195049
shalinigowsik <31505574+shalinigowsik@users.noreply.github.com>
2018-03-19 11:28:04 +0000

    Create hellos.c

---
+++ hellos.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[100];
+int l,mid;
+scanf("%s",&a);
+l=strlen(a);
+mid=l/2;
+if(l%2==0)
+{
+a[mid-1]='*';
+a[mid]='*';
+printf("%s",a);
+}
+else
+{
+a[mid]='*';
+printf("%s",a);
+}
+}
